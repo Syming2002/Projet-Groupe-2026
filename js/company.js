f2803953466c2561aa57d7d9f40a0a6c3dd8e20d
Syming2002 <simon.bruneau@yahoo.com>
2026-02-24 14:23:05 +0100

    Ajout de la version mobile de la page entreprise (à corriger)

--- js/company.js
+++ js/company.js
@@ -1,6 +1,37 @@
-const companyListDiv = document.querySelector(".company-list-div");
+const sidebar = document.querySelector(".sidebar");
+const burgerButton = document.querySelector(".fa-bars");
+const accessButton = document.querySelector(".fa-universal-access");
 
+const statutSelect = document.getElementById("statut-select");
+const postSelect = document.getElementById("post-select");
+const companySelect = document.getElementById("company-select");
+const dateSendSelect = document.getElementById("date-send-select");
+const dateResendSelect = document.getElementById("date-resend-select");
+
+const logoutButton = document.querySelector(".logout-button");
+
+const user = document.querySelector(".fa-user");
+
+function onBurgerButtonClick() {
+    if (burgerButton.classList.contains("fa-xmark")) {
+        burgerButton.classList.remove("fa-xmark");
+        burgerButton.classList.add("fa-bars");
+        burgerButton.classList.remove("acitve");
+        sidebar.classList.remove("active");
+        accessButton.classList.remove("active");
+    } else if (burgerButton.classList.contains("fa-bars")) {
+        burgerButton.classList.add("fa-xmark");
+        burgerButton.classList.remove("fa-bars");
+        burgerButton.classList.add("acitve");
+        sidebar.classList.add("active");
+        accessButton.classList.add("active");
+    }
+}
 
 function onWatchTracksButtonClick() {
     window.location.href = "/html/dashboard_page.html";
+}
+
+function onLogoutButtonClick() {
+    window.location.href = "/html/home_page.html";
 }
