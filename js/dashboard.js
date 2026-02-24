1b679206f1c7aa41d538bbcb561f1fef13f2a1e2
Syming2002 <simon.bruneau@yahoo.com>
2026-02-24 10:51:27 +0100

    Ajout de la version mobile de la page principale dashboard

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,3 +1,7 @@
+const sidebar = document.querySelector(".sidebar");
+const burgerButton = document.querySelector(".fa-bars");
+const accessButton = document.querySelector(".fa-universal-access");
+
 const statutSelect = document.getElementById("statut-select");
 const postSelect = document.getElementById("post-select");
 const companySelect = document.getElementById("company-select");
@@ -8,4 +12,18 @@ const logoutButton = document.querySelector(".logout-button");
 
 const user = document.querySelector(".fa-user");
 
-
+function onBurgerButtonClick() {
+    if (burgerButton.classList.contains("fa-xmark")) {
+        burgerButton.classList.remove("fa-xmark");
+        burgerButton.classList.add("fa-bars");
+        burgerButton.classList.remove("acitve");
+        sidebar.classList.remove("active");
+        accessButton.classList.remove("active");
+    } else if (burgerButton.classList.contains("fa-bars")) {
+        burgerButton.classList.add("fa-xmark");
+        burgerButton.classList.remove("fa-bars");
+        burgerButton.classList.add("acitve");
+        sidebar.classList.add("active");
+        accessButton.classList.add("active");
+    }
+}
