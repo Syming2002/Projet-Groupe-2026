79dc188847b191ad3da7795aed4304122fd76fbb
Syming2002 <simon.bruneau@yahoo.com>
2026-02-26 16:09:57 +0100

    Essaie de fetch les données d'un fichier .json

--- js/dashboard.js
+++ js/dashboard.js
@@ -4,7 +4,15 @@ const accessButton = document.querySelector(".fa-universal-access");
 
 const iceDiv = document.querySelector(".ice-div");
 
+const form = document.querySelector(".track-form");
 const formDiv = document.querySelector(".form-div");
+const statutInput = document.getElementById("statut-input");
+const postInput = document.getElementById("post-input");
+const companyInput = document.getElementById("company-input");
+const dateSendInput = document.getElementById("date-send-input");
+const dateResendInput = document.getElementById("date-resend-input");
+
+
 const trackButton = document.querySelector(".ice-add-button");
 
 const statutSelect = document.getElementById("statut-select");
@@ -16,6 +24,20 @@ const dateResendSelect = document.getElementById("date-resend-select");
 const logoutButton = document.querySelector(".logout-button");
 
 const user = document.querySelector(".fa-user");
+
+const dataArray = [];
+
+async function getData() {
+    try {
+        const fetch = await fetch("/json/list_company.json");
+
+        const data = await fetch.json();
+        dataArray = data;
+
+    } catch (error) {
+        console.error("Erreur: ", error);
+    }
+}
 
 function onBurgerButtonClick() {
     if (burgerButton.classList.contains("fa-xmark")) {
@@ -41,53 +63,108 @@ function onAddTrackButtonClick() {
         trackButton.textContent = '+';
         formDiv.style.display = "none";
     }
-}
 
-async function getJSONData() {
-    const root = "/json/list_statut_company.json";
-    try {
-        const response = await fetch(root);
-        if (!response.ok) {
-            throw new Error(`Statut de réponse: ${response.status}`);
+    const createBlackDiv = document.createElement("div");
+
+    const createDetailButton = document.createElement("button");
+
+    const createStatutText = document.createElement('p');
+    const createPostText = document.createElement('p');
+    const createCompanyText = document.createElement('p');
+    const createDateSendText = document.createElement('p');
+    const createResendDateText = document.createElement('p');
+
+    const createStatut = document.createElement('p');
+    const createPost = document.createElement('p');
+    const createCompany = document.createElement('p');
+    const createDateSend = document.createElement('p');
+    const createResendDate = document.createElement('p');
+
+    createBlackDiv.className = "black-div";
+
+    createDetailButton.className = "ice-add-button";
+    createDetailButton.textContent = "+";
+
+    createStatutText.className = "ice-text";
+
+    createPostText.className = "ice-text";
+    createCompanyText.className = "ice-text";
+    createDateSendText.className = "ice-text";
+    createResendDateText.className = "ice-text";
+    
+
+    dataArray.forEach((res) => {
+
+            createStatutText.textContent = res.statut
+            createPostText.textContent = res.poste_vise
+            createCompanyText.textContent = res.nom_entreprise
+            createDateSendText.textContent = res.date_envoi
+            createResendDateText.textContent = res.date_cloture
         }
+    );
 
-        const result = await response.json();
-        const companies = result.results.suivis;
-        companies.forEach(result => {
+    iceDiv.appendChild(createBlackDiv);
+    createBlackDiv.append(createStatutText, createPostText, createCompanyText, createDateSendText, createResendDateText ? createResendDateText : '-', createDetailButton);
 
-            const statutValue = result.statut;
-            const postValue = result.poste_vise;
-            const companyValue = result.nom_entreprise;
-            const dateSendValue = result.date_envoi;
+    
 
-            const createBlackDiv = document.createElement("div");
-            const createStatutText = document.createElement('p');
-            const createPostText = document.createElement('p');
-            const createCompanyText = document.createElement('p');
-            const createDateSendText = document.createElement('p');
-
-            createBlackDiv.style.backgroundColor = "#333";
-            createBlackDiv.style.width = "453px";
-            createBlackDiv.style.height = "200px";
-            createStatutText.textContent = statutValue;
-            createPostText.textContent = postValue;
-            createCompanyText.textContent = companyValue;
-            createDateSendText.textContent = dateSendValue;
-            
-
-            iceDiv.appendChild(createBlackDiv);
-            createBlackDiv.append(createStatutText, createPostText, createCompanyText, createDateSendText);
-
-            console.log("Black Div Created !!!!")
-        });
-        
-
-
-    } catch (error) {
-        console.error(error.message);
-    }
+    console.log("Black Div Created");
 }
 
-function onCreateTrackClick() {
-    getJSONData();
-}
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    // const statutValue = statutInput.value;
+    // const postValue = postInput.value;
+    // const companyValue = companyInput.value;
+    // const dateSendValue = dateSendInput.value;
+    // const dateResendValue = dateResendInput.value;
+
+    const createBlackDiv = document.createElement("div");
+
+    const createDetailButton = document.createElement("div");
+
+    const createStatutText = document.createElement('p');
+    const createPostText = document.createElement('p');
+    const createCompanyText = document.createElement('p');
+    const createDateSendText = document.createElement('p');
+    const createResendDateText = document.createElement('p');
+
+    createBlackDiv.className = "black-div";
+
+    createDetailButton.className = "ice-add-button";
+
+    createStatutText.className = "ice-text";
+    createPostText.className = "ice-text";
+    createCompanyText.className = "ice-text";
+    createDateSendText.className = "ice-text";
+    createResendDateText.className = "ice-text";
+
+
+    // createStatutText.textContent = statutValue;
+    // createPostText.textContent = postValue;
+    // createCompanyText.textContent = companyValue;
+    // createDateSendText.textContent = dateSendValue;
+    // createResendDateText.textContent = dateResendValue;
+    
+    const data = fetch("/json/list_company.json")
+        .then(res => res.json())
+        .then(data => data)
+        .catch(err => console.error(err));
+
+    const results = data.results;
+
+
+    iceDiv.appendChild(createBlackDiv);
+    createBlackDiv.append(createStatutText, createPostText, createCompanyText, createDateSendText, createResendDateText ? createResendDateText : '-');
+
+    results.forEach(res => 
+        createStatutText.textContent = res.statut,
+        createPostText.textContent = res.post_vise,
+        createCompanyText.textContent = res.nom_entreprise,
+        createDateSendText.textContent = res.date_envoi,
+        createResendDateText.textContent = res.date_cloture
+    );
+
+    console.log("Black Div Created !!!!");
+})
